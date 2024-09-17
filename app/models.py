26bc83011ff352409d10faed0fa521f7ccd83269
jnnalin <nalinjn1998@gmail.com>
2024-09-18 01:35:11 +0530

    created model for transactions table

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db import Base
+
+
+# Define Transactions table model
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float)
+    type = Column(String)
+    parent_id = Column(Integer, ForeignKey('transactions.id'))
+
+    parent = relationship("Transaction", remote_side=[id], backref="children")
